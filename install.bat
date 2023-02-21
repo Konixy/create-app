ca8af00e74345eb2d0e9d79af4aae0de260c047e
Konixy <konixy.p@gmail.com>
2023-02-21 15:17:15 +0100

    Update

--- install.bat
+++ install.bat
@@ -1,7 +1,14 @@
 @echo off
 
 echo Installing...
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Konixy/create-app/master/bin/create-app.exe -OutFile c:\Programs\create-app\create-app.exe"
 
-IF EXIST c:\Programs\create-app SET PATH=%PATH%;c:\Programs\create-app
+set dest="c:\Program Files\create-app"
+
+mkdir %dest%
+cd %dest%
+powershell -Command Invoke-WebRequest https://raw.githubusercontent.com/Konixy/create-app/master/bin/create-app.exe -OutFile ./create-app.exe
+
+IF EXIST %dest% SET PATH=%PATH%;c:\Program Files\create-app
+
 echo Installed successfully!
+pause
